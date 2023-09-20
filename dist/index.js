db698190d8c2b8b81b2413e082bbbccb972ca6d3
akimana-viateur <akimanaviateur94@gmail.com>
2023-09-20 10:07:16 +0200

    oop in typescript

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,24 @@
 "use strict";
-let age = 34;
-if (age < 50)
-    age += 10;
+class Person {
+    constructor(firstname, lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+    getFullname() {
+        return this.firstname + " " + this.lastname;
+    }
+}
+class Student extends Person {
+    constructor(studentId, firstname, lastname) {
+        super(firstname, lastname);
+        this.studentId = studentId;
+    }
+}
+class Teacher extends Person {
+    getFullname() {
+        return 'Tr' + this.firstname + " " + this.lastname;
+    }
+}
+let teacher = new Teacher("AKIMANA", "Viateur");
+console.log(teacher.getFullname());
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,85 @@
-function calculateTax(income: number,taxYear: number):number {
-    (taxYear < 2022)
-    return income * 1.2;
-    return income * 1.3
+// //creating classes
+// class Account {
+//     //  readonly id:number;
+//     // private owner:string;
+// //    private  balance:number;
+//     nickname?:string;
+// //parameter proterties
+//     constructor(
+//         public readonly id:number,
+//         private owner:string,
+//         private balance:number
+//         )
+//         {
+//         // this.id = id;
+//         // this.owner = owner;
+//         // this.balance = balance;
+//     }
+//     deposit(amount:number):void{
+//         if(amount <= 0){
+//             throw new Error("invalid amount");
+//             this.balance +=amount;
+            
+//         }
+
+//     }
+//     get Balance():number {
+//         return this.balance;
+//     }
+//     getOwner():string{
+//         return this.owner;
+//     }
+// }
+// //dealing with objects
+// const account = new Account(1,"viateur",-70);
+// console.log(account.balance)
+
+
+
+//index signatures
+// class SeatAssignment{
+    // using index signatures
+    // [SeatAssignment:string]:string;
+// }
+// 
+// object for assigning more properties
+// let seats = new SeatAssignment();
+// seats.A1 = "viateur";
+// seats.A2 = "fils";
+// seats.A3 ="arsene";
+// seats.A4 = "elite";
+// 
+// console.log(seats)
+
+
+    //inheritance
+
+class Person{
+    constructor(public firstname:string,public lastname:string){
+
+    }
+    getFullname(){
+        return this.firstname + " " + this.lastname;
+    }
 }
-calculateTax(60_000,2002);
+
+//derived classes 
+
+
+
+class Student extends Person{
+    constructor(public studentId:number, firstname:string,lastname:string){
+        super(firstname,lastname)
+    }
+}
+
+   //overriding
+
+
+class Teacher extends Person{
+   override getFullname(): string {
+        return 'Tr'+ " " + this.firstname + " " + this.lastname;
+    }
+}   
+let teacher = new Teacher("AKIMANA","Viateur");
+console.log(teacher.getFullname())
